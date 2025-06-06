ca1812ebd493c500f416df7e2ea7f8b344ccd1af
newuser <popovskaarisha123@gmail.com>
2025-06-06 19:01:31 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var boxer1 = 78.2;
-        var boxer2 =  82.7;
-
-        System.out.println(boxer2%boxer1);
+        var hours = 640;
+        System.out.println("Всего работников в компании — "+hours/8+" человек");
+        var people = hours/8 + 94;
+        System.out.println("Если в компании работает "+people + " человек, то всего " + people*8 + " часов работы может быть поделено между сотрудниками");
 
 
 
