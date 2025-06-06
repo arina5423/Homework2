069689621118e56b0b02cb0b229f56b122bceb33
newuser <popovskaarisha123@gmail.com>
2025-06-06 19:00:31 +0300

    Задача 6

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        var frog =3.5;
-        System.out.println(frog);
-        System.out.println(frog*10);
-        System.out.println(frog*10/3.5);
-        System.out.println(frog*10/3.5+4);
+        var boxer1 = 78.2;
+        var boxer2 =  82.7;
+        System.out.println(boxer1+boxer2+ " кг");
+        System.out.println(boxer2-boxer1 + " кг");
 
 
 
